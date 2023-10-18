3c61509f6e299c62af78ba642557dc45255c6ad4
Aaron Lambley <aaron.lambley@gmail.com>
2023-10-18 10:07:12 +0100

    add job processor module tests

---
+++ src/job-processor/test/job-processor.module.spec.ts
@@ -0,0 +1,83 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { JobProcessorModule } from '../job-processor.module';
+import { JobProcessorService } from '../../job-processor/job-processor.service';
+import { PrismaJobRepository } from '../../job/prisma-job.repository';
+import { mockPrismaJobRepository } from '../../../test/mocks/mockPrismaRepository';
+import { PrismaService } from '../../prisma.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { config } from '../../../config/configuration';
+import { getQueueToken } from '@nestjs/bull';
+
+const mockQueue = {
+  add: jest.fn(),
+};
+
+describe('JobProcessorModule', () => {
+  let module: TestingModule;
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [
+        JobProcessorModule,
+        ConfigModule.forRoot({
+          envFilePath: ['.env'],
+          load: [config],
+          isGlobal: true,
+        }),
+      ],
+      providers: [
+        JobProcessorService,
+        ConfigService,
+        PrismaService,
+        PrismaJobRepository,
+        {
+          provide: getQueueToken('jobQueue'),
+          useValue: mockQueue,
+        },
+      ],
+    })
+      .overrideProvider(PrismaJobRepository)
+      .useValue(mockPrismaJobRepository)
+      .compile();
+  });
+
+  it('should be defined', async () => {
+    expect(module).toBeDefined();
+  });
+
+  describe('imports', () => {
+    it('should import BullModule', () => {
+      const bullQueue = module.get(getQueueToken('jobQueue'));
+      expect(bullQueue).toBeDefined();
+    });
+  });
+
+  describe('providers', () => {
+    it('should provide JobProcessorService', () => {
+      const service = module.get<JobProcessorService>(JobProcessorService);
+      expect(service).toBeDefined();
+    });
+
+    it('should provide PrismaJobRepository', () => {
+      const service = module.get<PrismaJobRepository>(PrismaJobRepository);
+      expect(service).toBeDefined();
+    });
+
+    it('should provide PrismaService', () => {
+      const service = module.get<PrismaService>(PrismaService);
+      expect(service).toBeDefined();
+    });
+  });
+
+  describe('exports', () => {
+    it('should export JobProcessorService', () => {
+      const service = module.get<JobProcessorService>(JobProcessorService);
+      expect(service).toBeDefined();
+    });
+
+    it('should export BullModule', () => {
+      const bullQueue = module.get(getQueueToken('jobQueue'));
+      expect(bullQueue).toBeDefined();
+    });
+  });
+});
