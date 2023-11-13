f1dfb73bf47452874194b60c09e6ed297192eb6d
Aaron Lambley <aaron.lambley@gmail.com>
2023-11-13 10:43:39 +0000

    add app module import tests

--- src/test/app.module.spec.ts
+++ src/test/app.module.spec.ts
@@ -14,7 +14,32 @@ describe('AppModule', () => {
     expect(module).toBeDefined();
   });
 
-  describe('AppModule imports', () => {});
+  describe('AppModule imports', () => {
+    it('should import the ConfigModule', async () => {
+      const configModule = await module.resolve(AppModule);
+      expect(configModule).toBeDefined();
+    });
+
+    it('should import the JobModule', async () => {
+      const jobModule = await module.resolve(AppModule);
+      expect(jobModule).toBeDefined();
+    });
+
+    it('should import the BullModule', async () => {
+      const bullModule = await module.resolve(AppModule);
+      expect(bullModule).toBeDefined();
+    });
+
+    it('should import the JobProcessorModule', async () => {
+      const jobProcessorModule = await module.resolve(AppModule);
+      expect(jobProcessorModule).toBeDefined();
+    });
+
+    it('should import the JobConsumerModule', async () => {
+      const jobConsumerModule = await module.resolve(AppModule);
+      expect(jobConsumerModule).toBeDefined();
+    });
+  });
 
   describe('AppModule providers', () => {});
 });
