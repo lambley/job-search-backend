7d44c886592946105c29ddabc549db43799e4b86
Aaron Lambley <aaron.lambley@gmail.com>
2023-11-23 11:48:46 +0000

    add jobDbResultsFactory for database results

--- src/job/test/factories/jobFactory.ts
+++ src/job/test/factories/jobFactory.ts
@@ -1,6 +1,6 @@
 import { JobResponse, JobDbResponse } from 'src/job/types/job.interface';
 
-// for getJobs - returns an array of job responses
+// for refreshJobs - returns an array of job responses mocked from API
 const jobResultArrayFactory = (count: number): JobResponse[] => {
   const results: JobResponse[] = [];
 
@@ -48,4 +48,28 @@ const jobResultFactory = (): JobDbResponse => {
   };
 };
 
-export { jobResultArrayFactory, jobResultFactory };
+// for getJobs - mocks results from DB
+const jobDbResultsFactory = (count: number): JobDbResponse[] => {
+  const results: JobDbResponse[] = [];
+
+  for (let i = 0; i < count; i++) {
+    results.push({
+      id: i + 1,
+      adzuna_id: (1234567890 + i).toString(),
+      title: 'Job Title',
+      location: ['London'],
+      description: 'A job description',
+      created: '2021-01-01T00:00:00Z',
+      company: 'Company Name',
+      salary_min: 10000,
+      salary_max: 100000,
+      contract_type: 'full-time',
+      category: 'IT Jobs',
+      processed_keywords: ['keyword1', 'keyword2'],
+    });
+  }
+
+  return results;
+};
+
+export { jobResultArrayFactory, jobResultFactory, jobDbResultsFactory };
